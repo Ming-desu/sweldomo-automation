f6144c2c891a4c0aa2283b97ff7a13b0c5bbceab
Ming-desu <joshuaming.jm@gmail.com>
2025-06-04 18:54:30 +0800

    feat: added checking of node first before calling npm start

--- start.bat
+++ start.bat
@@ -1,3 +1,15 @@
 @echo off
+:waitForNode
+where node >nul 2>&1
+if errorlevel 1 (
+  echo Waiting for Node.js to be available...
+  timeout /t 3 >nul
+  goto waitForNode
+)
+
+REM Change to your Node.js project directory
+cd /d "C:\Users\Admin\Desktop\sweldomo-automation"
+
+REM Start the app
 echo Running automation...
-npm start
+call npm start
